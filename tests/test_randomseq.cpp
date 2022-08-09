521b06bbca634e06211968c89475be191fb59048
Enchan <enchant0130@gmail.com>
2022-08-09 19:08:40 +0900

    [Modify #10] add testcases

--- tests/test_randomseq.cpp
+++ tests/test_randomseq.cpp
@@ -5,7 +5,7 @@
 
 #include <randstr.hpp>
 
-// ランダム文字列の生成
+// 乱数の生成
 TEST(randstrTest, generateRandomValue) {
     uint32_t seed_1 = 0x12345678;
     uint32_t seed_2 = seed_1;
@@ -18,4 +18,18 @@ TEST(randstrTest, generateRandomValue) {
     seed_1 = seed_2;
     uint32_t num_2 = Random::random(&seed_1);
     EXPECT_EQ(num_1, num_2);
+}
+
+// 範囲付き
+TEST(randstrTest, generateRangedRandomValue) {
+    uint32_t seed = 0x12345678, *S = &seed;
+    const uint32_t min = 100, max = 200;
+
+    for (int i = 0; i < 100; i++){
+        uint32_t num = Random::randrange(S, min, max);
+        
+        EXPECT_GE(num, min);
+        EXPECT_LE(num, max);
+    }
+    
 }
