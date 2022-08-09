5c1e6370ca2df6e67ac2bdc4b4e17637dbad24e3
Enchan <enchant0130@gmail.com>
2022-08-09 19:08:25 +0900

    [Fix #10] random range

--- src/randstr.cpp
+++ src/randstr.cpp
@@ -8,7 +8,7 @@ namespace RandStr {
 std::string generate(uint32_t *seed, const std::string validChars, const unsigned int length) {
     std::string result = "";
     for (unsigned int i = 0; i < length; i++) {
-        uint32_t index = Random::randrange(seed, 0, validChars.size());
+        uint32_t index = Random::randrange(seed, 0, validChars.size() - 1);
         result += validChars[index];
     }
     return result;
