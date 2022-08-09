1bfd4591582f193d8b50ac969aecb1d59527e7ca
Enchan <enchant0130@gmail.com>
2022-08-09 14:36:48 +0900

    [Update #10] RandStr::generate

--- src/randstr.cpp
+++ src/randstr.cpp
@@ -5,10 +5,13 @@
 
 namespace RandStr {
 
-std::string generate(const std::string validChars, const unsigned int length){
-
-    return "";
+std::string generate(uint32_t *seed, const std::string validChars, const unsigned int length) {
+    std::string result = "";
+    for (unsigned int i = 0; i < length; i++) {
+        uint32_t index = Random::randrange(seed, 0, validChars.size());
+        result += validChars[index];
+    }
+    return result;
 }
-    
 
 }  // namespace RandStr
--- tests/test_randomseq.cpp
+++ tests/test_randomseq.cpp
@@ -3,10 +3,10 @@
 //
 #include <gtest/gtest.h>
 
-#include <random/random.hpp>
+#include <randstr.hpp>
 
-// 乱数の生成
-TEST(randomTest, generateRandomValue) {
+// ランダム文字列の生成
+TEST(randstrTest, generateRandomValue) {
     uint32_t seed_1 = 0x12345678;
     uint32_t seed_2 = seed_1;
 
@@ -18,18 +18,4 @@ TEST(randomTest, generateRandomValue) {
     seed_1 = seed_2;
     uint32_t num_2 = Random::random(&seed_1);
     EXPECT_EQ(num_1, num_2);
-}
-
-// 範囲付き
-TEST(randomTest, generateRangedRandomValue) {
-    uint32_t seed = 0x12345678, *S = &seed;
-
-    // 範囲を超えない
-    for (int i = 0; i < 100; i++)
-    {
-        uint32_t min = 0, max = 10000;
-        uint32_t num = Random::randrange(S, min, max);
-        EXPECT_GE(num, min);
-        EXPECT_LE(num, max);
-    }
 }
