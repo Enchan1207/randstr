2c033f431daa1e420561405d9317f5ac65e27172
Enchan <enchant0130@gmail.com>
2022-08-16 08:12:22 +0900

    [Update #12] add function to generate initial seed

--- src/randstr.cpp
+++ src/randstr.cpp
@@ -3,6 +3,9 @@
 //
 #include "randstr.hpp"
 
+#include <sys/time.h>
+#include <time.h>
+
 namespace RandStr {
 
 std::string generate(uint32_t *seed, const std::string validChars, const unsigned int length) {
@@ -11,6 +14,24 @@ std::string generate(uint32_t *seed, const std::string validChars, const unsigne
         uint32_t index = Random::randrange(seed, 0, validChars.size() - 1);
         result += validChars[index];
     }
+    return result;
+}
+
+int generate_initial_seed(uint32_t *S) {
+    // 現在時刻のμs部分を取得し、初期シードとする
+    struct timeval time;
+    int result = gettimeofday(&time, NULL);
+    if (result != 0) {
+        return result;
+    }
+    uint32_t seed = (uint32_t)time.tv_usec;
+
+    // 時刻情報だけだと連続処理する際に面倒なことになりそうなので、適当な法則に従って何度かシードを更新する
+    for (unsigned int i = 0; i < seed % 10; i++) {
+        Random::random(&seed);
+    }
+    *S = seed;
+
     return result;
 }
 
--- src/randstr.hpp
+++ src/randstr.hpp
@@ -5,20 +5,31 @@
 #ifndef _RANDSTR_
 #define _RANDSTR_
 
-#include <string>
 #include <random/random.hpp>
+#include <string>
 
 namespace RandStr {
 
 /**
  * @brief 有効文字列と文字数を指定して、ランダムな文字列を生成します。
- * 
+ *
  * @param validChars 生成する文字列に含めることができる文字の羅列
  * @param seed 乱数シード
  * @param length 生成する文字列の長さ
  * @return std::string 生成結果
  */
 std::string generate(uint32_t *seed, const std::string validChars, const unsigned int length);
+
+/**
+ * @brief 乱数生成器の初期シードを生成します。
+ *
+ * @param seed 生成されたシードが格納されるポインタ
+ * @return int 生成結果. 成功した場合は0, 失敗した場合は-1が返ります。
+ *
+ * @note 生成に失敗した場合は、*seedは更新されません。
+ *       シードの生成にtimeval構造体を使用しているため、呼び出しには1μs以上の間隔を開けることを推奨します。
+ */
+int generate_initial_seed(uint32_t *seed);
 
 }  // namespace RandStr
 
